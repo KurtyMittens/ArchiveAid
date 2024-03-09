0c4a0df635af6d5083c7f36fd11f52ed0b7a7670
KurtyMittens <134671520+KurtyMittens@users.noreply.github.com>
2024-03-09 22:26:02 +0800

    main: Unknown Property Boder

---
+++ AchiveAid.py
@@ -0,0 +1,415 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+class Ui_ArchiveAid(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+    def setupUi(self, ArchiveAid):
+        ArchiveAid.setObjectName("ArchiveAid")
+        ArchiveAid.setWindowModality(QtCore.Qt.WindowModal)
+        ArchiveAid.resize(450, 600)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(ArchiveAid.sizePolicy().hasHeightForWidth())
+        ArchiveAid.setSizePolicy(sizePolicy)
+        ArchiveAid.setMinimumSize(QtCore.QSize(450, 600))
+        ArchiveAid.setMaximumSize(QtCore.QSize(450, 600))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("kurtymittens/PythonFiles/FileOrganizer/assets/logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        ArchiveAid.setWindowIcon(icon)
+        ArchiveAid.setStyleSheet("background-color: rgb(251, 109, 108);")
+        ArchiveAid.setUnifiedTitleAndToolBarOnMac(False)
+        self.centralwidget = QtWidgets.QWidget(ArchiveAid)
+        self.centralwidget.setObjectName("centralwidget")
+        self.MenuButton = QtWidgets.QPushButton(self.centralwidget)
+        self.MenuButton.setGeometry(QtCore.QRect(10, 10, 51, 50))
+        self.MenuButton.setStyleSheet("QPushButton{\n"
+"    background-color: rgb(102, 111, 128);\n"
+"    border: 2px ;\n"
+"    border-radius: 15px;\n"
+"}\n"
+"QPushButton::hover{\n"
+"    background-color: rgb(102, 111, 128);\n"
+"    border: 2px solid #C3C8D3;\n"
+"    border-radius: 15px;\n"
+"}")
+        self.MenuButton.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("../assets/menu.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.MenuButton.setIcon(icon1)
+        self.MenuButton.setIconSize(QtCore.QSize(50, 50))
+        self.MenuButton.setCheckable(False)
+        self.MenuButton.setObjectName("MenuButton")
+        self.TitleFrame = QtWidgets.QFrame(self.centralwidget)
+        self.TitleFrame.setGeometry(QtCore.QRect(70, 10, 371, 55))
+        self.TitleFrame.setStyleSheet("#TitleFrame{\n"
+"    background-color: rgb(102, 111, 128);\n"
+"    border:2px;\n"
+"border-radius: 25px;\n"
+"\n"
+"}")
+        self.TitleFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.TitleFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.TitleFrame.setObjectName("TitleFrame")
+        self.title_label = QtWidgets.QLabel(self.TitleFrame)
+        self.title_label.setGeometry(QtCore.QRect(90, 7, 251, 41))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NFM SemiBold")
+        font.setPointSize(28)
+        font.setBold(True)
+        font.setWeight(75)
+        self.title_label.setFont(font)
+        self.title_label.setStyleSheet("#title_label{\n"
+"    background-color: rgb(102, 111, 128);\n"
+"    \n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        self.title_label.setObjectName("title_label")
+        self.Logo_pixmap = QtWidgets.QLabel(self.TitleFrame)
+        self.Logo_pixmap.setGeometry(QtCore.QRect(20, 7, 51, 41))
+        self.Logo_pixmap.setStyleSheet("background-color: rgb(102, 111, 128);")
+        self.Logo_pixmap.setText("")
+        self.Logo_pixmap.setObjectName("Logo_pixmap")
+        self.FileFrame = QtWidgets.QFrame(self.centralwidget)
+        self.FileFrame.setGeometry(QtCore.QRect(10, 70, 431, 521))
+        self.FileFrame.setStyleSheet("#FileFrame{\n"
+"    background-color: rgb(102, 111, 128);\n"
+"    border:2px;\n"
+"    border-radius: 20px;\n"
+"\n"
+"}")
+        self.FileFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.FileFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.FileFrame.setObjectName("FileFrame")
+        self.scrollArea = QtWidgets.QScrollArea(self.FileFrame)
+        self.scrollArea.setGeometry(QtCore.QRect(20, 20, 401, 431))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.scrollArea.sizePolicy().hasHeightForWidth())
+        self.scrollArea.setSizePolicy(sizePolicy)
+        self.scrollArea.setStyleSheet(" QWidget {\n"
+"        border: none;\n"
+"        background-color: rgb(102, 111, 128);\n"
+"    }\n"
+"\n"
+"    QScrollBar {\n"
+"        background: rgb(195, 200, 211);\n"
+"        border-radius: 5px;\n"
+"    }\n"
+"\n"
+"    QScrollBar:horizontal {\n"
+"        height: 5px;\n"
+"    }\n"
+"\n"
+"    QScrollBar:vertical {\n"
+"        width: 5px;\n"
+"    }\n"
+"\n"
+"    QScrollBar::handle {\n"
+"        background: #FB6D6C;\n"
+"        border-radius: 5px;\n"
+"    }\n"
+"\n"
+"    QScrollBar::handle:horizontal {\n"
+"        height: 5px;\n"
+"        min-width: 5px;\n"
+"    }\n"
+"\n"
+"    QScrollBar::handle:vertical {\n"
+"        width: 5px;\n"
+"        min-height: 5px;\n"
+"    }\n"
+"\n"
+"")
+        self.scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setAlignment(QtCore.Qt.AlignCenter)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 401, 431))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.File_frames = QtWidgets.QFrame(self.scrollAreaWidgetContents)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.File_frames.sizePolicy().hasHeightForWidth())
+        self.File_frames.setSizePolicy(sizePolicy)
+        self.File_frames.setMinimumSize(QtCore.QSize(380, 90))
+        self.File_frames.setStyleSheet("background-color: rgb(195, 200, 211);\n"
+"boder:2px;\n"
+"border-radius:25px;")
+        self.File_frames.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.File_frames.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.File_frames.setObjectName("File_frames")
+        self.Filename_label = QtWidgets.QLabel(self.File_frames)
+        self.Filename_label.setGeometry(QtCore.QRect(100, 25, 151, 41))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NFM SemiBold")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.Filename_label.setFont(font)
+        self.Filename_label.setStyleSheet("background-color: rgb(195, 200, 211);\n"
+"color: rgb(0, 0, 0);\n"
+"")
+        self.Filename_label.setObjectName("Filename_label")
+        self.extension_select = QtWidgets.QComboBox(self.File_frames)
+        self.extension_select.setGeometry(QtCore.QRect(290, 40, 51, 21))
+        self.extension_select.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"color:rgb(0, 0, 0);\n"
+"")
+        self.extension_select.setEditable(True)
+        self.extension_select.setCurrentText("")
+        self.extension_select.setPlaceholderText("")
+        self.extension_select.setObjectName("extension_select")
+        self.extension_label = QtWidgets.QLabel(self.File_frames)
+        self.extension_label.setGeometry(QtCore.QRect(280, 20, 71, 21))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NFM SemiBold")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.extension_label.setFont(font)
+        self.extension_label.setStyleSheet("color: rgb(0, 0, 0);")
+        self.extension_label.setObjectName("extension_label")
+        self.file_pixmap = QtWidgets.QLabel(self.File_frames)
+        self.file_pixmap.setGeometry(QtCore.QRect(10, 10, 81, 71))
+        self.file_pixmap.setStyleSheet("background-color: rgb(102, 111, 128);")
+        self.file_pixmap.setText("")
+        self.file_pixmap.setObjectName("file_pixmap")
+        self.verticalLayout.addWidget(self.File_frames)
+        self.AddFrameBtn = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.AddFrameBtn.sizePolicy().hasHeightForWidth())
+        self.AddFrameBtn.setSizePolicy(sizePolicy)
+        self.AddFrameBtn.setMinimumSize(QtCore.QSize(380, 90))
+        self.AddFrameBtn.setStyleSheet("QPushButton{\n"
+"    background-color: rgb(195, 200, 211);\n"
+"    border: 2px ;\n"
+"    border-radius: 15px;\n"
+"}\n"
+"QPushButton::pressed{\n"
+"    background-color:  rgb(195, 200, 211);\n"
+"    border: 10px solid  #666f80;\n"
+"    border-radius: 15px;\n"
+"}")
+        self.AddFrameBtn.setText("")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("../assets/add.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.AddFrameBtn.setIcon(icon2)
+        self.AddFrameBtn.setIconSize(QtCore.QSize(50, 50))
+        self.AddFrameBtn.setCheckable(False)
+        self.AddFrameBtn.setObjectName("AddFrameBtn")
+        self.verticalLayout.addWidget(self.AddFrameBtn)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.OrganizeBtn = QtWidgets.QPushButton(self.FileFrame)
+        self.OrganizeBtn.setGeometry(QtCore.QRect(150, 460, 131, 51))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NF SemiBold")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.OrganizeBtn.setFont(font)
+        self.OrganizeBtn.setStyleSheet("QPushButton{\n"
+"    background-color: rgb(251, 109, 108);\n"
+"    border:2px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(255, 255, 255);\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"    background-color: rgb(255, 255, 255);\n"
+"    color: rgb(251, 109, 108);\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"    background-color: rgb(195, 200, 211);\n"
+"    color:rgb(102, 111, 128)\n"
+"}")
+        self.OrganizeBtn.setObjectName("OrganizeBtn")
+        self.OptionFrame = QtWidgets.QFrame(self.centralwidget)
+        self.OptionFrame.setGeometry(QtCore.QRect(0, 0, 251, 601))
+        self.OptionFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.OptionFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.OptionFrame.setObjectName("OptionFrame")
+        self.OptionFrame.hide()
+        self.back_button = QtWidgets.QPushButton(self.OptionFrame)
+        self.back_button.setGeometry(QtCore.QRect(190, 10, 51, 50))
+        self.back_button.setStyleSheet("QPushButton{\n"
+"    background-color: rgb(102, 111, 128);\n"
+"    border: 2px ;\n"
+"    border-radius: 15px;\n"
+"}\n"
+"QPushButton::hover{\n"
+"    background-color: rgb(102, 111, 128);\n"
+"    border: 2px solid #C3C8D3;\n"
+"    border-radius: 15px;\n"
+"}")
+        self.back_button.setText("")
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap("../assets/back.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.back_button.setIcon(icon3)
+        self.back_button.setIconSize(QtCore.QSize(50, 50))
+        self.back_button.setCheckable(False)
+        self.back_button.setObjectName("back_button")
+        self.option_label = QtWidgets.QLabel(self.OptionFrame)
+        self.option_label.setGeometry(QtCore.QRect(20, 15, 161, 41))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NFM SemiBold")
+        font.setPointSize(28)
+        font.setBold(True)
+        font.setWeight(75)
+        self.option_label.setFont(font)
+        self.option_label.setStyleSheet("color: rgb(255, 255, 255);\n"
+"")
+        self.option_label.setObjectName("option_label")
+        self.OptionSelection = QtWidgets.QFrame(self.OptionFrame)
+        self.OptionSelection.setGeometry(QtCore.QRect(10, 70, 231, 521))
+        self.OptionSelection.setStyleSheet("#OptionSelection{\n"
+"    background-color: rgb(102, 111, 128);\n"
+"    border:2px;\n"
+"    border-radius: 20px;\n"
+"\n"
+"}")
+        self.OptionSelection.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.OptionSelection.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.OptionSelection.setObjectName("OptionSelection")
+        self.historychace = QtWidgets.QPushButton(self.OptionSelection)
+        self.historychace.setGeometry(QtCore.QRect(10, 250, 210, 70))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.historychace.sizePolicy().hasHeightForWidth())
+        self.historychace.setSizePolicy(sizePolicy)
+        self.historychace.setMinimumSize(QtCore.QSize(210, 70))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NF SemiBold")
+        font.setBold(True)
+        font.setWeight(75)
+        self.historychace.setFont(font)
+        self.historychace.setStyleSheet("QPushButton{\n"
+"    background-color: rgb(195, 200, 211);\n"
+"    border:2 px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(0, 0, 0);\n"
+"}\n"
+"QPushButton::hover{\n"
+"    background-color: rgb(251, 109, 108);\n"
+"    border:2 px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(0, 0, 0);\n"
+"}")
+        self.historychace.setObjectName("historychace")
+        self.pickundoselaercion = QtWidgets.QPushButton(self.OptionSelection)
+        self.pickundoselaercion.setGeometry(QtCore.QRect(10, 170, 210, 70))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pickundoselaercion.sizePolicy().hasHeightForWidth())
+        self.pickundoselaercion.setSizePolicy(sizePolicy)
+        self.pickundoselaercion.setMinimumSize(QtCore.QSize(210, 70))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NF SemiBold")
+        font.setBold(True)
+        font.setWeight(75)
+        self.pickundoselaercion.setFont(font)
+        self.pickundoselaercion.setStyleSheet("QPushButton{\n"
+"    background-color: rgb(195, 200, 211);\n"
+"    border:2 px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(0, 0, 0);\n"
+"}\n"
+"QPushButton::hover{\n"
+"    background-color: rgb(251, 109, 108);\n"
+"    border:2 px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(0, 0, 0);\n"
+"}")
+        self.pickundoselaercion.setObjectName("pickundoselaercion")
+        self.undo_last_button = QtWidgets.QPushButton(self.OptionSelection)
+        self.undo_last_button.setGeometry(QtCore.QRect(10, 90, 210, 70))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.undo_last_button.sizePolicy().hasHeightForWidth())
+        self.undo_last_button.setSizePolicy(sizePolicy)
+        self.undo_last_button.setMinimumSize(QtCore.QSize(210, 70))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NF SemiBold")
+        font.setBold(True)
+        font.setWeight(75)
+        self.undo_last_button.setFont(font)
+        self.undo_last_button.setStyleSheet("QPushButton{\n"
+"    background-color: rgb(195, 200, 211);\n"
+"    border:2 px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(0, 0, 0);\n"
+"}\n"
+"QPushButton::hover{\n"
+"    background-color: rgb(251, 109, 108);\n"
+"    border:2 px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(0, 0, 0);\n"
+"}")
+        self.undo_last_button.setObjectName("undo_last_button")
+        self.file_sourceselection = QtWidgets.QPushButton(self.OptionSelection)
+        self.file_sourceselection.setGeometry(QtCore.QRect(10, 10, 210, 70))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.file_sourceselection.sizePolicy().hasHeightForWidth())
+        self.file_sourceselection.setSizePolicy(sizePolicy)
+        self.file_sourceselection.setMinimumSize(QtCore.QSize(210, 70))
+        font = QtGui.QFont()
+        font.setFamily("JetBrainsMono NF SemiBold")
+        font.setBold(True)
+        font.setWeight(75)
+        self.file_sourceselection.setFont(font)
+        self.file_sourceselection.setStyleSheet("QPushButton{\n"
+"    background-color: rgb(195, 200, 211);\n"
+"    border:2 px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(0, 0, 0);\n"
+"}\n"
+"QPushButton::hover{\n"
+"    background-color: rgb(251, 109, 108);\n"
+"    border:2 px;\n"
+"    border-radius:25px;\n"
+"    color:rgb(0, 0, 0);\n"
+"}")
+        self.file_sourceselection.setObjectName("file_sourceselection")
+        ArchiveAid.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(ArchiveAid)
+        QtCore.QMetaObject.connectSlotsByName(ArchiveAid)
+
+    def retranslateUi(self, ArchiveAid):
+        _translate = QtCore.QCoreApplication.translate
+        ArchiveAid.setWindowTitle(_translate("ArchiveAid", "ArchiveAid"))
+        self.title_label.setText(_translate("ArchiveAid", "ARCHIVE/AID"))
+        self.Filename_label.setText(_translate("ArchiveAid", "Filename"))
+        self.extension_label.setText(_translate("ArchiveAid", "Extension"))
+        self.OrganizeBtn.setText(_translate("ArchiveAid", "ORGANIZE"))
+        self.option_label.setText(_translate("ArchiveAid", "OPTIONS"))
+        self.historychace.setText(_translate("ArchiveAid", "HISTORY CACHE"))
+        self.pickundoselaercion.setText(_translate("ArchiveAid", "UNDO SELECTED \n"
+"ORGANIZATION"))
+        self.undo_last_button.setText(_translate("ArchiveAid", "UNDO LAST \n"
+"ORGANIZATION"))
+        self.file_sourceselection.setText(_translate("ArchiveAid", "CURRENT FILE SOURCE:"))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    run = Ui_ArchiveAid()
+    sys.exit(app.exec_())
